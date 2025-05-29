f3c2c7e3e85e1f537a3cdad33f4931c64fb0b60a
Pauline Thomas <pauline.thomas.lgc@gmail.com>
2025-05-29 12:10:41 +0200

    Refactoring function infinteCounter and commentaries

--- assets/script.js
+++ assets/script.js
@@ -27,12 +27,13 @@ const slides = [
 	}
 ]
 
-// crée variable compteur i
+// crée variable compteur i :
 i = 0
-// crée variable n, nombre d'éléments dans le tableau slides
+
+// crée variable n, nombre d'éléments dans le tableau slides :
 let n = slides.length
 
-// Récupére les éléments div, img et p du html
+// Récupére les éléments div, img et p du html (bannière, titre, flêches) :
 const bannerContent = document.getElementById("banner")
 let bannerImg = document.querySelector(".banner-img")
 let bannerTitle = document.querySelector("#banner p")
@@ -40,7 +41,7 @@ let bannerTitle = document.querySelector("#banner p")
 const arrowLeft = document.querySelector('.arrow_left')
 const arrowRight = document.querySelector('.arrow_right')
 
-// crée le même nombre de span dots que de slides dans le html et leur attribue la classe css .dot
+// crée le même nombre de span dots que de slides dans le html et leur attribue la classe css ".dot" :
 for(let dot of slides) {
 	let dot = document.createElement("span")
 	const dotsContainer = document.querySelector(".dots")
@@ -48,20 +49,30 @@ for(let dot of slides) {
 	dot.classList.add("dot")
 }
 
-// récupère les span avec la classe .dot dans la variable selectedDot
+// récupère les span avec la classe CSS ".dot" dans la variable selectedDot :
 let selectedDot = document.querySelectorAll('.dot')
 
-// attribue par défaut, la class dot_selected à la span dot d'index i, i étant par défaut égal à zéro
+// attribue par défaut, la class dot_selected à la span dot d'index i, i étant par défaut égal à zéro (ligne 31) :
 selectedDot[i].classList.add("dot_selected")
 
-// crée fonction modification de l'image et du titre en fonction de i
+// crée fonction modification de l'image et du titre en fonction de i :
 function slider(i) {
 	bannerImg.src = `../assets/images/slideshow/${slides[i].image}`
 	bannerTitle.innerHTML = `${slides[i].tagLine}`
 	selectedDot[i].classList.add("dot_selected")
 }
+/* Fonctions */
+// force i au retour à la valeur n - 1 s'il atteint -1 (dernière slide) et à la valeur zéro s'il atteint n 
+// (première slide), créant ainsi une boucle infinie vers la gouche comme vers la droite
+function infiniteCounter(i) {
+	if (i < 0) {
+			i = i + n
+		}  else if (i === n) {
+			i = 0
+		}
+}
 
-// ajoute ou enlève la classe dot_selected en fonction de la valeur de i
+// ajoute ou enlève la classe dot_selected en fonction de la valeur de i :
 function activeDotLeft(i) {
 	if (i < n-1 && i >=0) {
 			selectedDot[i+1].classList.remove("dot_selected")
@@ -78,16 +89,11 @@ function activeDotRight(i) {
 			selectedDot[n - 1].classList.remove("dot_selected")
 		}
 }
-
-//créer les évênements clic déclenchant les fonctions
+/* Déclencheur de fonctions */
+//créer les évênements déclenchant les fonctions au clic sur les flêches gauche ou droite
 arrowLeft.addEventListener("click", () => {
 	i= i - 1
-	//infiniteCounter(i)
-	if (i < 0) {
-			i = i + (n)
-		} else if (i === n) {
-			i = 0
-		}
+	infiniteCounter(i)
 	slider(i)
 	activeDotLeft(i)
 	}
@@ -95,12 +101,7 @@ arrowLeft.addEventListener("click", () => {
 
 arrowRight.addEventListener("click", () => {
 	i = i + 1
-	//infiniteCounter(i)
-		if (i < 0) {
-			i = i + n
-		}  else if (i === n) {
-			i = 0
-		}
+	infiniteCounter(i)
 	slider(i)
 	activeDotRight(i)
 	}
